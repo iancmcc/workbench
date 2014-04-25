ebfb5b9de339e75194fa4849f67e36d1fff19706
Ian McCracken <ian.mccracken@gmail.com>
2014-04-25 16:49:26 -0500

    Add parsing from a file

--- jigfile.go
+++ jigfile.go
@@ -3,6 +3,7 @@ package jig
 import (
 	"encoding/json"
 	"io"
+	"os"
 )
 
 type Jigfile struct {
@@ -14,6 +15,7 @@ type Build struct {
 	Build  []string
 	Post   []string
 	Output []string
+	Image  string
 }
 
 func ParseJigfile(r io.Reader) (*Jigfile, error) {
@@ -23,4 +25,16 @@ func ParseJigfile(r io.Reader) (*Jigfile, error) {
 		return nil, err
 	}
 	return jf, nil
+}
+
+func ParseJigfileFromFile(p string) (*Jigfile, error) {
+	var (
+		f   io.ReadCloser
+		err error
+	)
+	if f, err = os.Open(p); err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return ParseJigfile(f)
 }
--- jigfile_test.go
+++ jigfile_test.go
@@ -1,7 +1,10 @@
 package jig_test
 
 import (
+	"io/ioutil"
+	"os"
 	"strings"
+	"syscall"
 
 	. "github.com/iancmcc/jig"
 
@@ -11,7 +14,7 @@ import (
 
 var _ = Describe("Jigfile", func() {
 
-	Describe("Parsing a Jigfile", func() {
+	Describe("Parsing Jigfile data", func() {
 
 		var (
 			data  string
@@ -39,7 +42,8 @@ var _ = Describe("Jigfile", func() {
 						],
 						"output": [
 							"myfile.tgz"
-						]
+						],
+						"image": "jigs/test"
 					}
 				}
 				`
@@ -74,6 +78,9 @@ var _ = Describe("Jigfile", func() {
 						[]string{"myfile.tgz"},
 					))
 				})
+				It("should have an image", func() {
+					Expect(build.Image).To(Equal("jigs/test"))
+				})
 			})
 
 			It("should not error", func() {
@@ -81,6 +88,84 @@ var _ = Describe("Jigfile", func() {
 			})
 		})
 
+	})
+
+	Describe("Parsing a Jigfile", func() {
+		var (
+			file  *os.File
+			err   error
+			jf    *Jigfile
+			build Build
+		)
+		BeforeEach(func() {
+			data := `
+				{
+					"ubuntu": {
+						"pre": [
+							"/bin/bash dependencies.sh"
+						],
+						"build": [
+							"configure", 
+							"make", 
+							"make install"
+						],
+						"post": [
+							"/bin/bash cleanup.sh"
+						],
+						"output": [
+							"myfile.tgz"
+						],
+						"image": "jigs/test"
+					}
+				}
+				`
+			file, _ = ioutil.TempFile("", "Jigfile")
+			file.WriteString(data)
+			file.Sync()
+			jf, err = ParseJigfileFromFile(file.Name())
+		})
+		AfterEach(func() {
+			if file != nil {
+				syscall.Unlink(file.Name())
+				file = nil
+			}
+		})
+		It("should have the build by name", func() {
+			Expect(jf.Builds).To(HaveKey("ubuntu"))
+		})
+
+		Context("the build", func() {
+			BeforeEach(func() {
+				build = jf.Builds["ubuntu"]
+			})
+			It("should have a pre script", func() {
+				Expect(build.Pre).To(Equal(
+					[]string{"/bin/bash dependencies.sh"},
+				))
+			})
+			It("should have build commands", func() {
+				Expect(build.Build).To(Equal(
+					[]string{"configure", "make", "make install"},
+				))
+			})
+			It("should have post commands", func() {
+				Expect(build.Post).To(Equal(
+					[]string{"/bin/bash cleanup.sh"},
+				))
+			})
+			It("should have output artifacts", func() {
+				Expect(build.Output).To(Equal(
+					[]string{"myfile.tgz"},
+				))
+			})
+			It("should have an image", func() {
+				Expect(build.Image).To(Equal("jigs/test"))
+			})
+		})
+
+		It("should not error", func() {
+			Expect(err).NotTo(HaveOccurred())
+		})
 	})
 
 })
